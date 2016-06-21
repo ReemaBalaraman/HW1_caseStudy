9a8fca6167e60feb9e5d5883ba39a30b8317936f
Reema Balaraman”4git config --global user.name Reema <mac123>
2016-06-20 23:28:37 -0500

    Added test case for map sorting method

--- src/main/java/ValueComparator.java
+++ src/main/java/ValueComparator.java
@@ -3,18 +3,18 @@ package main.java;
 import java.util.Comparator;
 import java.util.HashMap;
 
-	/* a comparator for sorting maps */
-	class ValueComparator<K, V extends Comparable<V>> implements Comparator<K>{
+/* a comparator for sorting maps */
+public	class ValueComparator<K, V extends Comparable<V>> implements Comparator<K>{
 
-		HashMap<K, V> map = new HashMap<K, V>();
+	HashMap<K, V> map = new HashMap<K, V>();
 
-		public ValueComparator(HashMap<K, V> map){
-			this.map.putAll(map);
-		}
-
-		@Override
-		public int compare(K s1, K s2) {
-			return map.get(s1).compareTo(map.get(s2));//ascending order	
-		}
+	public ValueComparator(HashMap<K, V> map){
+		this.map.putAll(map);
 	}
+
+	@Override
+	public int compare(K s1, K s2) {
+		return map.get(s1).compareTo(map.get(s2));//ascending order	
+	}
+}
 
--- src/test/java/TestProcess.java
+++ src/test/java/TestProcess.java
@@ -2,34 +2,52 @@ package test.java;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import main.java.Process;
+import main.java.ValueComparator;
 
 public class TestProcess {
-	
-private Process process;
-	
+
+	private Process process;
+	private ValueComparator<String,Float> valueComparator;
+	private HashMap<String,Float> map ;
 	@Before
 	public void setUp() throws Exception {
 		process = new Process();
+		map = new HashMap<String,Float>();
+		valueComparator = new ValueComparator<String,Float>(map);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		process = null;
+		valueComparator = null;
 	}
-	
+
 	@Test
 	public void getzipCodes() {
 		assertNotNull(process);
 		assertNotNull( process.zipCodeMap());
 	}
+
+	@Test
+	public void sortMap() {
+		assertNotNull(process);
+		map.put("first", 50.23f);
+		map.put("second", 35.23f);
+		valueComparator = new ValueComparator<String,Float>(map);
+		int i = valueComparator.compare("first","second");
+		assertEquals(1,i);
+	}
+
 	@Test
 	public void readXML() {
 		assertNotNull(process);
-		 process.readXML();
+		process.readXML();
 	}
+
 }
